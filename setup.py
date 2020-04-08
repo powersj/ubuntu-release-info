7e4b2df16106adb617f3c472dfecdc507ae8533e
Robin Winslow <robin@robinwinslow.co.uk>
2020-04-08 11:55:41 +0100

    Update to v20.2

--- setup.py
+++ setup.py
@@ -4,29 +4,29 @@ import os
 from setuptools import find_packages, setup
 
 PWD = os.path.abspath(os.path.dirname(__name__))
-REQUIREMENTS_FILE = os.path.join(PWD, 'requirements.txt')
+REQUIREMENTS_FILE = os.path.join(PWD, "requirements.txt")
 REQUIREMENTS = []
-with open(REQUIREMENTS_FILE, 'r') as req_file:
+with open(REQUIREMENTS_FILE, "r") as req_file:
     REQUIREMENTS = req_file.read().splitlines()
 
-README_FILE = os.path.join(PWD, 'README.md')
-with open(README_FILE, 'r') as readme:
+README_FILE = os.path.join(PWD, "README.md")
+with open(README_FILE, "r") as readme:
     README_TEXT = readme.read()
 
 setup(
-    name='ubuntu-release-info',
-    version='20.1',
-    description='Ubuntu distribution release information',
+    name="ubuntu-release-info",
+    version="20.2",
+    description="Ubuntu distribution release information",
     long_description=README_TEXT,
-    long_description_content_type='text/markdown',
-    author='Joshua Powers',
-    author_email='josh.powers@canonical.com',
-    url='https://github.com/powersj/ubuntu-release-info',
+    long_description_content_type="text/markdown",
+    author="Joshua Powers",
+    author_email="josh.powers@canonical.com",
+    url="https://github.com/powersj/ubuntu-release-info",
     download_url=(
-        'https://github.com/powersj/ubuntu-release-info/tarball/master'
+        "https://github.com/powersj/ubuntu-release-info/tarball/master"
     ),
     install_requires=REQUIREMENTS,
-    python_requires='>=3.4',
+    python_requires=">=3.4",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
@@ -41,12 +41,13 @@ setup(
         "Topic :: Software Development :: Quality Assurance",
         "Topic :: Software Development :: Testing",
     ],
-    keywords=['ubuntu', 'distro', 'release', 'info'],
+    keywords=["ubuntu", "distro", "release", "info"],
     packages=find_packages(),
     include_package_data=True,
     entry_points={
-        'console_scripts':
-            ['ubuntu-release-info=ubuntu_release_info.__main__:launch']
+        "console_scripts": [
+            "ubuntu-release-info=ubuntu_release_info.__main__:launch"
+        ]
     },
-    zip_safe=True
+    zip_safe=True,
 )
